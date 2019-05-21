2668252988573689a8df7effa2ec054a41c24e9d
zhulinqingtian <1399873403@qq.com>
2019-05-21 13:57:38 +0800

    Add: vuex的两种引入方法

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,101 +1,56 @@
 <template>
   <div class="hello">
-    <h2>Essential Links {{$store.state.name}}</h2>
-    <ul style="display: none">
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul style="display: none">
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="info-block">
+      <p>click {{count}} times</p>
+      <button @click="increment">增加</button>
+      <button @click="decrement">减少</button>
+      <button @click="incrementIfOdd">是奇数是增加</button>
 
-    <p>当前值：{{$store.state.count}}</p>
-    <Button type='primary' @click="$store.commit('add')">+</Button>
-    <button type='primary' @click="$store.commit('reduce')">-</button>
+      <button @click="_getUserInfo">获取用户信息</button>
+      <button @click="_getPlatInfo">获取平台信息</button>
+    </div>
+
+    <div class="info-block">
+      用户名：{{currentUser.name}}
+      角色： {{currentUser.role || ''}}
+      <br>
+      平台名称：{{plat && plat.name || ''}}
+      版本：{{plat && plat.version}}
+      更新时间： {{plat && plat.updateTime}}
+    </div>
+    <router-link to="/view/optimizeVuex">优化过后的vuex的使用</router-link>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
+import { mapState } from 'vuex'
 import API from './../axios/api'
+import commonMethods from '../assets/utils/commonMethods.js'
 
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      newsListShow: []
+      newsListShow: [],
+      currentUser: {
+        name: 'test',
+        role: 'test01'
+      }
     }
+  },
+  computed: {
+    count () {
+      return this.$store.state.count
+    },
+    user () {
+      return this.$store.state.user
+    },
+    plat () {
+      return this.$store.state.plat
+    }
+  },
+  watch: {
+    user: 'changeUser' // 检测user对象变化，检测方法：changeUser
   },
   mounted () {
     this.getNewList()
@@ -110,9 +65,29 @@ export default {
         .catch(error => {
           this.$Message.error(error)
         })
+    },
+    changeUser (user) {
+      if (user && !commonMethods.isEmptyObject(user)) {
+        this.currentUser = user
+      }
+    },
+    increment () {
+      this.$store.commit('INCREMENT')
+    },
+    decrement () {
+      this.$store.commit('DECREMENT')
+    },
+    // 只有是奇数才加1
+    incrementIfOdd () {
+      this.$store.dispatch('incrementIfOdd') // 触发store中对应的action调用
+    },
+    _getUserInfo () {
+      this.$store.dispatch('getUserInfo')
+    },
+    _getPlatInfo () {
+      this.$store.dispatch('getPlatInfo')
     }
-  },
-  computed: mapState(['count'])
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import * as getters from './getters'
+import actions from './actions'
+import mutations from './mutations'
 Vue.use(Vuex)
 
 const state = {
-  name: '123',
-  count: 1
-}
-
-/* eslint no-unused-vars: 0 */
-const mutations = {
-  add (state) {
-    state.count += 1
+  user: {
+    name: '',
+    role: '',
+    id: ''
   },
-  reduce (state) {
-    state.count -= 1
+  count: 0,
+  plat: {
+    name: '',
+    version: '1.0.0',
+    updateTime: '2017-1-4'
   }
 }
 
 export default new Vuex.Store({
-  state
+  state,
+  mutations,
+  actions,
+  getters
 })
